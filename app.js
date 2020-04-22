8e6c81c266c07ff96db8a36dec9dc1ad16e9f197
sertomviz <sertom20@gmail.com>
2020-04-22 15:19:30 +0200

    minor changes

--- app.js
+++ app.js
@@ -12,37 +12,3 @@ fs.createReadStream("/dev/ttyUSB0")
   .on('data', function(data) {
       console.log(data)
   })
-  .on('error', function(err) {
-    console.log(err)
-    fd.close()
-  })
-
-// async function processLineByLine() {
-//   //const sr = fs.createReadStream('/dev/ttyUSB0');
-//   const sr = fs.createReadStream('/home/toms/lineberry.json');
-//
-//   // const rl = readline.createInterface({
-//   //   input: fileStream,
-//   //   crlfDelay: Infinity
-//   // });
-//   // Note: we use the crlfDelay option to recognize all instances of CR LF
-//   // ('\r\n') in input.txt as a single line break.
-//
-//   // for await (const line of rl) {
-//   //   console.log(line);
-//   // }
-//
-//   sr.setEncoding('utf8');
-//   sr.on('data', function (data) {
-//       console.log(data)
-//   });
-//
-// }
-
-//processLineByLine();
-
-// const SerialPort = require('serialport')
-//
-// SerialPort.list().then(ports => {
-//   console.log('ports', ports);
-// })
